15d3d97e7cceb1d8c93daf08fca32d8438cb1edd
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-11-26 19:03:29 +0100

    port to libgpiod

--- src/rrda.ts
+++ src/rrda.ts
@@ -1,39 +1,11 @@
-import { Gpio } from 'onoff';
-import { PERCENTAGES } from './constants.js';
-// TODO: gpio and devices should be setup by device config
-const bindings = [5, 10, 20, 30, 50];
+import gpiod from 'node-libgpiod'
+import { PERCENTAGES } from './constants.js'
 
-const gpios: {[index: number]: Gpio} = {
-  5: new Gpio(26, 'out'),
-  10: new Gpio(6, 'out'),
-  20: new Gpio(5, 'out'),
-  30: new Gpio(22, 'out'),
-  50: new Gpio(27, 'out'),
-}
+const { Chip, Line } = gpiod
 
-const devices = [12, 20, 21].map(pin => new Gpio(pin, 'out'))
+const bindings = [5, 10, 20, 30, 50]
 
-const write = (gpio: Gpio, state: 1 | 0) => gpio.writeSync(state)
-
-/**
- * Turn off all devices except the ones in the percentage array
- * @param percentage number
- *
- * @example
- * ifnotPercentage(5) // 10, 20, 30, 50
- */
-const ifnotPercentage = (percentage: number | number[]) => {
-  if (!Array.isArray(percentage)) percentage = [percentage]
-
-  const off = bindings.reduce((p, c) => {
-    if (percentage.indexOf(c) === -1) p.push(c);
-    return p;
-  }, [] as number[])
-
-  for (const value of off) write(gpios[value], 0)
-
-  if (percentage[0] !== 0) for (const value of percentage) write(gpios[value], 1)
-}
+const write = (line: gpiod.Line, state: 1 | 0) => line.setValue(state)
 
 /**
  *
@@ -48,71 +20,112 @@ const ifnotPercentage = (percentage: number | number[]) => {
  */
 const byFive = (percentage: number) => {
   return PERCENTAGES.reduce((p, c) => {
-    if (c <= percentage) return c;
-    else return p;
-  }, 0);
-};
+    if (c <= percentage) return c
+    else return p
+  }, 0)
+}
+export class RRDADevice {
+  chip: gpiod.Chip
+  lines: { [index: number]: gpiod.Line } = {}
+  devices: gpiod.Line[] = []
 
-/**
- * @param percentage number
- * @returns void
- *
- * @example
- * dim(5) // 5
- */
-export const dim = (percentage: number) => {
-  percentage = byFive(percentage);
-  if (percentage === 0) {
-    return ifnotPercentage(0)
-  } else if (percentage === 5 ||
-             percentage === 10 ||
-             percentage === 20 ||
-             percentage === 30 ||
-             percentage === 50) {
-    return ifnotPercentage(percentage);
-  } else if (percentage === 15) {
-    return ifnotPercentage([5, 10]);
-  } else if (percentage === 25) {
-    return ifnotPercentage([5, 20]);
-  } else if (percentage === 35) {
-    return ifnotPercentage([5, 30]);
-  } else if (percentage === 40) {
-    return ifnotPercentage([10, 30]);
-  } else if (percentage === 45) {
-    return ifnotPercentage([5, 10, 30]);
-  } else if (percentage === 55) {
-    return ifnotPercentage([5, 50]);
-  } else if (percentage === 60) {
-    return ifnotPercentage([10, 50]);
-  } else if (percentage === 65) {
-    return ifnotPercentage([5, 10, 50]);
-  } else if (percentage === 70) {
-    return ifnotPercentage([20, 50]);
-  } else if (percentage === 75) {
-    return ifnotPercentage([5, 20, 50]);
-  } else if (percentage === 80) {
-    return ifnotPercentage([30, 50]);
-  } else if (percentage === 85) {
-    return ifnotPercentage([5, 30, 50]);
-  } else if (percentage === 90) {
-    return ifnotPercentage([10, 30, 50]);
-  } else if (percentage === 95) {
-    return ifnotPercentage([5, 10, 30, 50]);
-  } else if (percentage === 100) {
-    return ifnotPercentage([20, 30, 50]);
+  constructor() {
+    this.chip = new Chip(0)
+
+    this.lines = {
+      5: new Line(this.chip, 26),
+      10: new Line(this.chip, 6),
+      20: new Line(this.chip, 5),
+      30: new Line(this.chip, 22),
+      50: new Line(this.chip, 27)
+    }
+
+    this.devices = [12, 20, 21].map((pin) => new Line(this.chip, pin))
+
+    for (const line of Object.values(this.lines)) {
+      line.requestOutputMode()
+    }
+
+    for (const device of this.devices) {
+      device.requestOutputMode()
+    }
   }
-}
 
-/**
- * @param {number} device [1] 1, 2 or 3
- */
-export const on = (device = 1) => {
-  write(devices[Number(device) - 1], 1)
-}
+  // TODO: gpio and devices should be setup by device config
 
-/**
- * @param {number} device [1] 1, 2 or 3
- */
-export const off = (device = 1) => {
-  write(devices[Number(device) - 1], 0)
+  /**
+   * Turn off all devices except the ones in the percentage array
+   * @param percentage number
+   *
+   * @example
+   * ifnotPercentage(5) // 10, 20, 30, 50
+   */
+  ifnotPercentage(percentage: number | number[]) {
+    if (!Array.isArray(percentage)) percentage = [percentage]
+
+    const off = bindings.reduce((p, c) => {
+      if (percentage.indexOf(c) === -1) p.push(c)
+      return p
+    }, [] as number[])
+
+    for (const value of off) write(this.lines[value], 0)
+
+    if (percentage[0] !== 0) for (const value of percentage) write(this.lines[value], 1)
+  }
+
+  /**
+   * @param percentage number
+   * @returns void
+   *
+   * @example
+   * dim(5) // 5
+   */
+  dim(percentage: number) {
+    percentage = byFive(percentage)
+    if (percentage === 0) {
+      return this.ifnotPercentage(0)
+    } else if (percentage === 5 || percentage === 10 || percentage === 20 || percentage === 30 || percentage === 50) {
+      return this.ifnotPercentage(percentage)
+    } else if (percentage === 15) {
+      return this.ifnotPercentage([5, 10])
+    } else if (percentage === 25) {
+      return this.ifnotPercentage([5, 20])
+    } else if (percentage === 35) {
+      return this.ifnotPercentage([5, 30])
+    } else if (percentage === 40) {
+      return this.ifnotPercentage([10, 30])
+    } else if (percentage === 45) {
+      return this.ifnotPercentage([5, 10, 30])
+    } else if (percentage === 55) {
+      return this.ifnotPercentage([5, 50])
+    } else if (percentage === 60) {
+      return this.ifnotPercentage([10, 50])
+    } else if (percentage === 65) {
+      return this.ifnotPercentage([5, 10, 50])
+    } else if (percentage === 70) {
+      return this.ifnotPercentage([20, 50])
+    } else if (percentage === 75) {
+      return this.ifnotPercentage([5, 20, 50])
+    } else if (percentage === 80) {
+      return this.ifnotPercentage([30, 50])
+    } else if (percentage === 85) {
+      return this.ifnotPercentage([5, 30, 50])
+    } else if (percentage === 90) {
+      return this.ifnotPercentage([10, 30, 50])
+    } else if (percentage === 95) {
+      return this.ifnotPercentage([5, 10, 30, 50])
+    } else if (percentage === 100) {
+      return this.ifnotPercentage([20, 30, 50])
+    }
+  }
+
+  on(device: number = 1, percentage?: number) {
+    if (percentage) this.dim(percentage)
+    else write(this.devices[Number(device) - 1], 1)
+    write(this.devices[Number(device) - 1], 1)
+  }
+
+  off(device: number = 1) {
+    write(this.devices[Number(device) - 1], 0)
+  }
 }
