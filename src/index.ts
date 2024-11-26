59e851aad8ee08dff7062f9a7b7cbfd6a96ae2b7
VandeurenGlenn <8685280+VandeurenGlenn@users.noreply.github.com>
2024-11-26 19:03:14 +0100

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,65 @@
-import mqtt from "mqtt";
-import { BRIGHTNESS_COMMAND_TOPIC, BRIGHTNESS_STATE_TOPIC, COMMAND_TOPIC, CONFIG_TOPIC, DEVICE_INFO, OFF, ON, STATE_TOPIC } from "./constants.js";
-const client = mqtt.connect("mqtt://test.mosquitto.org", {
-  clientId: DEVICE_INFO.unique_id,
-  // username: "rrda",
-  // password: "rrda"
-});
+import mqtt from 'mqtt'
+import { env } from 'process'
+import { RRDADevice } from './rrda.js'
+import {
+  BRIGHTNESS_COMMAND_TOPIC,
+  BRIGHTNESS_STATE_TOPIC,
+  COMMAND_TOPIC,
+  CONFIG_TOPIC,
+  DEVICE_INFO,
+  OFF,
+  ON,
+  STATE_TOPIC
+} from './constants.js'
+import { readState, writeState } from './helpers.js'
 
-import {on, off, dim} from './rrda.js'
+const state = await readState()
 
-client.on("connect", () => {
-  client.subscribe("homeassistant/status", (err) => {
-    client.publish(CONFIG_TOPIC, JSON.stringify(DEVICE_INFO));
-  });
-  client.publish(CONFIG_TOPIC, JSON.stringify(DEVICE_INFO));
-    client.publish(STATE_TOPIC, ON);
-    client.publish(BRIGHTNESS_STATE_TOPIC, '100');
-  client.subscribe(COMMAND_TOPIC);
-  client.subscribe(BRIGHTNESS_COMMAND_TOPIC);
-});
+const device = new RRDADevice()
 
-client.on("message", (topic, message) => {
-  // message is Buffer
-  console.log({topic});
+const client = mqtt.connect('mqtt://test.mosquitto.org', {
+  // clientId: DEVICE_INFO.unique_id,
+  username: env.USERNAME,
+  password: env.PASSWORD
+})
 
-  const payload = message.toString();
-  console.log({payload});
+client.on('connect', () => {
+  console.log('Connected')
 
-  if (topic === "homeassistant/status" && message.toString() === "online") {
-    client.publish(CONFIG_TOPIC, JSON.stringify(DEVICE_INFO));
-    client.publish(STATE_TOPIC, ON);
-    client.publish(BRIGHTNESS_STATE_TOPIC, '100');
+  client.subscribe('homeassistant/status', (err) => {
+    console.log('Connected/status')
+    client.publish(CONFIG_TOPIC, JSON.stringify(DEVICE_INFO))
+  })
+  client.publish(CONFIG_TOPIC, JSON.stringify(DEVICE_INFO))
+  client.publish(STATE_TOPIC, ON)
+  client.publish(BRIGHTNESS_STATE_TOPIC, '100')
+  client.subscribe(COMMAND_TOPIC)
+  client.subscribe(BRIGHTNESS_COMMAND_TOPIC)
+})
+
+client.on('message', (topic, message) => {
+  console.log('message', topic, message.toString())
+  const payload = message.toString()
+
+  if (topic === 'homeassistant/status' && message.toString() === 'online') {
+    client.publish(CONFIG_TOPIC, JSON.stringify(DEVICE_INFO))
+    client.publish(STATE_TOPIC, ON)
+    client.publish(BRIGHTNESS_STATE_TOPIC, '100')
   } else if (topic === COMMAND_TOPIC) {
     if (payload === ON) {
-      // on()
-      client.publish(STATE_TOPIC, ON);
+      state.on = true
+      device.on()
+      client.publish(STATE_TOPIC, ON)
     } else {
-      // off()
-      client.publish(STATE_TOPIC, OFF);
+      state.on = false
+      device.off()
+      client.publish(STATE_TOPIC, OFF)
     }
+    writeState(state)
   } else if (topic === BRIGHTNESS_COMMAND_TOPIC) {
-    // dim(parseInt(payload));
-    client.publish(BRIGHTNESS_STATE_TOPIC, payload);
+    state.brightness = parseInt(payload)
+    device.dim(state.brightness)
+    client.publish(BRIGHTNESS_STATE_TOPIC, payload)
+    writeState(state)
   }
-});
+})
